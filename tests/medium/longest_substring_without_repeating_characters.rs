4340ce3e30dfc54dd43e6a30581b38896cdf709a
Bruce Tsai <bruce.tasi@gmail.com>
2022-07-10 23:46:48 +0000

    3. Longest Substring Without Repeating Characters

---
+++ tests/medium/longest_substring_without_repeating_characters.rs
@@ -0,0 +1,52 @@
+// # 3. Longest Substring Without Repeating Characters
+use std::{cmp::max, collections::HashSet};
+
+struct Solution;
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut m = 0;
+        for (i1, c1) in s.chars().enumerate() {
+            let mut set: HashSet<char> = HashSet::new();
+            set.insert(c1);
+
+            for (_, c2) in s.char_indices().skip(i1 + 1) {
+                if set.contains(&c2) {
+                    break;
+                } else {
+                    set.insert(c2);
+                }
+            }
+
+            m = max(m, set.len());
+        }
+
+        m as i32
+    }
+}
+
+#[test]
+fn test_length_of_longest_substring() {
+    let input = "abcabcbb";
+    let result = Solution::length_of_longest_substring(input.to_owned());
+    assert_eq!(3, result, "input: {:?}", input);
+
+    let input = "bbbbb";
+    let result = Solution::length_of_longest_substring(input.to_owned());
+    assert_eq!(1, result, "input: {:?}", input);
+
+    let input = "pwwkew";
+    let result = Solution::length_of_longest_substring(input.to_owned());
+    assert_eq!(3, result, "input: {:?}", input);
+
+    let input = " ";
+    let result = Solution::length_of_longest_substring(input.to_owned());
+    assert_eq!(1, result, "input: {:?}", input);
+
+    let input = "abcabcbb";
+    let result = Solution::length_of_longest_substring(input.to_owned());
+    assert_eq!(3, result, "input: {:?}", input);
+
+    let input = "au";
+    let result = Solution::length_of_longest_substring(input.to_owned());
+    assert_eq!(2, result, "input: {:?}", input);
+}
--- tests/medium/mod.rs
+++ tests/medium/mod.rs
@@ -1,1 +1,2 @@
 mod add_two_numbers; // #2
+mod longest_substring_without_repeating_characters; // #3
