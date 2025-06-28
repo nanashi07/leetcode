295e7d5aa7b6f2a70296470fd440703404ad4721
Bruce Tsai <bruce.tsai@opennet.tw>
2025-06-28 01:11:33 +0000

    Add problem 2099

---
+++ tests/easy/find_subsequence_of_length_k_with_the_largest_sum.rs
@@ -0,0 +1,9 @@
+// # 2099. Find Subsequence of Length K With the Largest Sum
+// https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+struct Solution;
+
+impl Solution {
+    pub fn max_subsequence(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        todo!()
+    }
+}
