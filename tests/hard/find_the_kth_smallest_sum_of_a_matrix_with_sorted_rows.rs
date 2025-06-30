0a9b1a0c6eb8a7909f07f695ec862128d000be0c
Bruce Tsai <bruce.tsai@opennet.tw>
2025-06-30 00:51:05 +0000

    Implementing problem 1439

--- tests/hard/find_the_kth_smallest_sum_of_a_matrix_with_sorted_rows.rs
+++ tests/hard/find_the_kth_smallest_sum_of_a_matrix_with_sorted_rows.rs
@@ -4,8 +4,112 @@
 struct Solution;
 
 impl Solution {
+    pub fn _kth_smallest3(mat: Vec<Vec<i32>>, k: i32) -> i32 {
+        let outer_len = mat.len();
+        let inner_len = mat[0].len();
+        println!("mat: {:?}", &mat);
+
+        let mut sums: Vec<i32> = Vec::new();
+        let mut indexes = vec![0; outer_len];
+
+        println!("sums: {:?}", &sums);
+        *sums.iter().take(k as usize).last().unwrap()
+    }
+
     pub fn kth_smallest(mat: Vec<Vec<i32>>, k: i32) -> i32 {
-        todo!()
+        let outer_len = mat.len();
+        let inner_len = mat[0].len();
+        let mut diff: Vec<Vec<i32>> = vec![vec![0; inner_len]; outer_len];
+        println!("mat: {:?}", &mat);
+
+        for o in 0..outer_len {
+            for i in 1..inner_len {
+                diff[o][i] = mat[o][i] - mat[o][0];
+            }
+        }
+        println!("diff: {:?}", &diff);
+
+        let mut indexes = vec![0; outer_len];
+        let mut sums: Vec<i32> = Vec::new();
+        sums.push(mat.iter().fold(0, |acc, x| acc + x[0]));
+
+        while (sums.len() as i32) <= 2 * k {
+            // println!("indexes: {:?}", &indexes);
+
+            // iterator all possible next sums
+            for changing_row in 0..outer_len {
+                let mut base = sums[0];
+                let mut temp: Vec<i32> = Vec::new();
+                for i in 0..outer_len {
+                    // iterator changing row all remained elements
+                }
+                println!("temp: {:?}, sum = {}", &temp, base);
+                sums.push(base);
+            }
+
+            // move index to the next min value
+            let mut p = 0;
+            let mut min = i32::MAX;
+            for i in 0..outer_len {
+                let next = indexes[i] + 1;
+                if next < inner_len && min > mat[i][next] {
+                    p = i;
+                    min = mat[i][next];
+                }
+            }
+
+            indexes[p] += 1;
+            let mut moved: Vec<i32> = Vec::new();
+            for i in 0..outer_len {
+                moved.push(mat[i][indexes[i]])
+            }
+            println!("Move to {:?}, index: {:?}", &moved, &indexes);
+            // println!("update indexes: {:?}", &indexes);
+        }
+
+        sums.sort();
+        println!("sums: {:?}", &sums);
+        *sums.iter().take(k as usize).last().unwrap()
+    }
+
+    // too slow
+    pub fn _kth_smallest(mat: Vec<Vec<i32>>, k: i32) -> i32 {
+        // let mut heap: BinaryHeap<i32> = BinaryHeap::new();
+        let mut heap: Vec<i32> = Vec::new();
+        let outer_len = mat.len();
+        let inner_len = mat[0].len();
+        println!("mat: {:?}", &mat);
+
+        let mut indexes = vec![0; outer_len];
+
+        while indexes[0] < inner_len {
+            let mut sum = 0;
+            let mut sub: Vec<i32> = Vec::new();
+
+            // println!("index: {:?}", &indexes);
+
+            for i in (0..outer_len).rev() {
+                let value = mat[i][indexes[i]];
+                sum += value;
+                sub.insert(0, value);
+            }
+
+            // index + 1, individual rounding
+            indexes[outer_len - 1] += 1;
+            for i in (1..outer_len).rev() {
+                if indexes[i] >= inner_len {
+                    indexes[i - 1] += 1;
+                    indexes[i] = 0;
+                }
+            }
+
+            heap.push(sum);
+            println!("{:?}, sum = {}", sub, sum);
+        }
+
+        heap.sort();
+        println!("heap: {:?}", &heap);
+        *heap.iter().take(k as usize).last().unwrap()
     }
 }
 
@@ -15,22 +119,70 @@ mod tests {
 
     #[test]
     fn test_kth_smallest_1() {
-        let mat = vec![vec![1, 3, 11], vec![2, 4, 6]];
+        let mat = [[1, 3, 11], [2, 4, 6]]
+            .iter()
+            .map(|e| e.to_vec())
+            .collect::<Vec<_>>();
         let k = 5;
         assert_eq!(7, Solution::kth_smallest(mat, k));
     }
 
     #[test]
     fn test_kth_smallest_2() {
-        let mat = vec![vec![1, 3, 11], vec![2, 4, 6]];
+        let mat = [[1, 3, 11], [2, 4, 6]]
+            .iter()
+            .map(|e| e.to_vec())
+            .collect::<Vec<_>>();
         let k = 9;
         assert_eq!(17, Solution::kth_smallest(mat, k));
     }
 
     #[test]
     fn test_kth_smallest_3() {
-        let mat = vec![vec![1, 10, 10], vec![1, 4, 5], vec![2, 3, 6]];
+        let mat = [[1, 10, 10], [1, 4, 5], [2, 3, 6]]
+            .iter()
+            .map(|e| e.to_vec())
+            .collect::<Vec<_>>();
         let k = 7;
+        assert_eq!(9, Solution::kth_smallest(mat, k));
+    }
+
+    #[test]
+    fn test_kth_smallest_4() {
+        let mat = [
+            [21, 33, 38, 41, 42, 44, 48, 54, 62],
+            [8, 51, 56, 60, 80, 81, 83, 88, 92],
+            [2, 40, 54, 56, 68, 92, 93, 94, 98],
+            [28, 35, 41, 52, 55, 59, 62, 70, 99],
+            [2, 16, 25, 30, 32, 38, 54, 73, 92],
+            [1, 11, 14, 35, 35, 55, 59, 66, 66],
+            [5, 9, 13, 29, 47, 49, 60, 73, 74],
+            [19, 29, 32, 36, 59, 64, 66, 98, 100],
+        ]
+        .iter()
+        .map(|e| e.to_vec())
+        .collect::<Vec<_>>();
+        let k = 29;
+        assert_eq!(141, Solution::kth_smallest(mat, k));
+    }
+
+    #[test]
+    fn test_kth_smallest_5() {
+        let mat = [[1, 10, 10], [1, 4, 5], [2, 3, 6]]
+            .iter()
+            .map(|e| e.to_vec())
+            .collect::<Vec<_>>();
+        let k = 14;
+        assert_eq!(16, Solution::kth_smallest(mat, k));
+    }
+
+    #[test]
+    fn test_kth_smallest_6() {
+        let mat = [[2, 5, 6], [1, 5, 8], [2, 3, 7]]
+            .iter()
+            .map(|e| e.to_vec())
+            .collect::<Vec<_>>();
+        let k = 4;
         assert_eq!(9, Solution::kth_smallest(mat, k));
     }
 }
