cbbb8adacae25f02eb563f086d90a9436aa69563
Bruce Tsai <bruce.tsai@opennet.tw>
2025-07-03 00:15:15 +0000

    implement problem 1439

--- tests/hard/find_the_kth_smallest_sum_of_a_matrix_with_sorted_rows.rs
+++ tests/hard/find_the_kth_smallest_sum_of_a_matrix_with_sorted_rows.rs
@@ -4,7 +4,7 @@
 struct Solution;
 
 impl Solution {
-    pub fn _kth_smallest3(mat: Vec<Vec<i32>>, k: i32) -> i32 {
+    pub fn kth_smallest(mat: Vec<Vec<i32>>, k: i32) -> i32 {
         let outer_len = mat.len();
         let inner_len = mat[0].len();
         println!("mat: {:?}", &mat);
@@ -12,22 +12,78 @@ impl Solution {
         let mut sums: Vec<i32> = Vec::new();
         let mut indexes = vec![0; outer_len];
 
+        let take_sum = |slices: &Vec<usize>| {
+            let mut sum = 0;
+            let mut sub: Vec<i32> = Vec::new();
+            for i in 0..outer_len {
+                sub.push(mat[i][slices[i]]);
+                sum += mat[i][slices[i]];
+            }
+            println!("sub: {:?}, sum = {}", &sub, sum);
+            sum
+        };
+
+        sums.sort();
+        let mut max_sum = 0;
+
+        let init_sum = take_sum(&indexes);
+        while (sums.len() < k as usize) || max_sum >= init_sum {
+            // println!("indexes: {:?}", &indexes);
+            sums.push(init_sum);
+
+            for moving_down in 0..outer_len {
+                let index = indexes[moving_down];
+
+                for moving_right in index + 1..inner_len {
+                    let mut another_indexes = indexes.clone();
+                    another_indexes[moving_down] = moving_right;
+
+                    let another_sum = take_sum(&another_indexes);
+                    if (sums.len() < k as usize) || another_sum <= max_sum {
+                        sums.push(another_sum);
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            // move index to the next min value
+            let mut p = 0;
+            let mut min = i32::MAX;
+            for i in 0..outer_len {
+                let next = indexes[i] + 1;
+                if next < inner_len && min > mat[i][next] {
+                    p = i;
+                    min = mat[i][next];
+                }
+            }
+
+            indexes[p] += 1;
+            let mut moved: Vec<i32> = Vec::new();
+            for i in 0..outer_len {
+                moved.push(mat[i][indexes[i]])
+            }
+            println!("Move to {:?}, index: {:?}", &moved, &indexes);
+
+            sums.sort();
+            max_sum = *sums.iter().take(k as usize).max().unwrap();
+        }
         println!("sums: {:?}", &sums);
         *sums.iter().take(k as usize).last().unwrap()
     }
 
-    pub fn kth_smallest(mat: Vec<Vec<i32>>, k: i32) -> i32 {
+    pub fn kth_smallest4(mat: Vec<Vec<i32>>, k: i32) -> i32 {
         let outer_len = mat.len();
         let inner_len = mat[0].len();
-        let mut diff: Vec<Vec<i32>> = vec![vec![0; inner_len]; outer_len];
         println!("mat: {:?}", &mat);
+        // let mut diff: Vec<Vec<i32>> = vec![vec![0; inner_len]; outer_len];
 
-        for o in 0..outer_len {
-            for i in 1..inner_len {
-                diff[o][i] = mat[o][i] - mat[o][0];
-            }
-        }
-        println!("diff: {:?}", &diff);
+        // for o in 0..outer_len {
+        //     for i in 1..inner_len {
+        //         diff[o][i] = mat[o][i] - mat[o][0];
+        //     }
+        // }
+        // println!("diff: {:?}", &diff);
 
         let mut indexes = vec![0; outer_len];
         let mut sums: Vec<i32> = Vec::new();
@@ -37,14 +93,23 @@ impl Solution {
             // println!("indexes: {:?}", &indexes);
 
             // iterator all possible next sums
+
             for changing_row in 0..outer_len {
-                let mut base = sums[0];
-                let mut temp: Vec<i32> = Vec::new();
                 for i in 0..outer_len {
+                    let mut base = sums[0];
+                    let mut temp: Vec<i32> = Vec::new();
+
                     // iterator changing row all remained elements
+                    if i == changing_row {
+                        // iterator to the end
+                        for j in indexes[i]..inner_len {}
+                    } else {
+                        base += mat[i][indexes[i]];
+                    }
+
+                    println!("temp: {:?}, sum = {}", &temp, base);
+                    sums.push(base);
                 }
-                println!("temp: {:?}, sum = {}", &temp, base);
-                sums.push(base);
             }
 
             // move index to the next min value
