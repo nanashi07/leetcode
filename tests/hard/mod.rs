68d508a8462cf9a93cb70722601d5e51907bbebf
Bruce Tsai <bruce.tsai@opennet.tw>
2025-07-13 00:29:11 +0000

    3333

--- tests/hard/mod.rs
+++ tests/hard/mod.rs
@@ -9,10 +9,10 @@ mod reverse_nodes_in_k_group;
 // # 30. Substring with Concatenation of All Words
 mod substring_with_concatenation_of_all_words;
 // # 32. Longest Valid Parentheses
-mod find_the_original_typed_string_ii;
-// # 3333. Find the Original Typed String II
 mod longest_valid_parentheses;
 // # 1900. The Earliest and Latest Rounds Where Players Compete
 mod the_earliest_and_latest_rounds_where_players_compete;
 // # 3307. Find the K-th Character in String Game II
 mod find_the_k_th_character_in_string_game_ii;
+// # 3333. Find the Original Typed String II
+mod find_the_original_typed_string_ii;
