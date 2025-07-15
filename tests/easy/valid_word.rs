80106e74bc28fbc6adfe973621bd385bba7e7cb5
Bruce Tsai <bruce.tsai@opennet.tw>
2025-07-15 02:19:40 +0000

    Solve problem 3136

--- tests/easy/valid_word.rs
+++ tests/easy/valid_word.rs
@@ -5,7 +5,35 @@ struct Solution;
 
 impl Solution {
     pub fn is_valid(word: String) -> bool {
-        todo!()
+        if word.len() < 3 {
+            return false;
+        }
+
+        let mut vc = 0;
+        let mut cc = 0;
+
+        // 0-9: 48 - 57
+        // A-Z: 65 - 90
+        // a-z: 97 - 122
+        // vowel: 65, 69, 73, 79, 85
+        let vowel = [65, 69, 73, 79, 85];
+
+        for c in word.chars() {
+            let n = c as u32;
+            if (n >= 48 && n <= 57) || (n >= 65 && n <= 90) || (n >= 97 && n <= 122) {
+                if n >= 65 {
+                    if vowel.contains(&n) || vowel.contains(&(n + 65 - 97)) {
+                        vc += 1;
+                    } else {
+                        cc += 1;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+
+        vc > 0 && cc > 0
     }
 }
 
