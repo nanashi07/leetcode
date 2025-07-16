9e09307d3a819bb3766dea3af6539e432a9583ea
Bruce Tsai <bruce.tsai@opennet.tw>
2025-07-16 00:53:17 +0000

    Problem 3201

---
+++ tests/medium/find_the_maximum_length_of_valid_subsequence_i.rs
@@ -0,0 +1,33 @@
+// # 3201. Find the Maximum Length of Valid Subsequence I
+// https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-i/
+
+struct Solution;
+
+impl Solution {
+    pub fn maximum_length(nums: Vec<i32>) -> i32 {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::medium::find_the_maximum_length_of_valid_subsequence_i::Solution;
+
+    #[test]
+    fn test_maximum_length_1() {
+        let nums = [1, 2, 3, 4].to_vec();
+        assert_eq!(4, Solution::maximum_length(nums));
+    }
+
+    #[test]
+    fn test_maximum_length_2() {
+        let nums = [1, 2, 1, 1, 2, 1, 2].to_vec();
+        assert_eq!(6, Solution::maximum_length(nums));
+    }
+
+    #[test]
+    fn test_maximum_length_3() {
+        let nums = [1, 3].to_vec();
+        assert_eq!(2, Solution::maximum_length(nums));
+    }
+}
