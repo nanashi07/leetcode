5529545fb07ab93a558002795ad8fb1fcbb56759
Bruce Tsai <bruce.tsai@opennet.tw>
2025-07-16 11:09:17 +0000

    Solve problem 3201

--- tests/medium/find_the_maximum_length_of_valid_subsequence_i.rs
+++ tests/medium/find_the_maximum_length_of_valid_subsequence_i.rs
@@ -5,7 +5,32 @@ struct Solution;
 
 impl Solution {
     pub fn maximum_length(nums: Vec<i32>) -> i32 {
-        todo!()
+        println!("num: {:?}", &nums);
+
+        let mut all_even_max = 0;
+        let mut all_odds_max = 0;
+        let mut even_odds_max = 1;
+
+        for i in 0..nums.len() {
+            let even = nums[i] % 2 == 0;
+
+            if even {
+                all_even_max += 1;
+                if i > 0 && nums[i - 1] % 2 == 1 {
+                    even_odds_max += 1;
+                }
+            } else {
+                all_odds_max += 1;
+                if i > 0 && nums[i - 1] % 2 == 0 {
+                    even_odds_max += 1;
+                }
+            }
+        }
+
+        *[all_even_max, all_odds_max, even_odds_max]
+            .iter()
+            .max()
+            .unwrap()
     }
 }
 
