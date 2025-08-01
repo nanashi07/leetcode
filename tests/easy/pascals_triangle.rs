b9e142d6022677a7bfd7e7a3e54642aac0b5c26f
Bruce Tsai <bruce.tsai@opennet.tw>
2025-08-01 11:54:06 +0000

    Solve problem 118

--- tests/easy/pascals_triangle.rs
+++ tests/easy/pascals_triangle.rs
@@ -5,7 +5,30 @@ struct Solution;
 
 impl Solution {
     pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
-        todo!()
+        let mut rows: Vec<Vec<i32>> = Vec::new();
+
+        for _ in 1..=num_rows {
+            if rows.len() == 0 {
+                rows.push(vec![1])
+            } else {
+                let last = &rows[rows.len() - 1];
+                let len = last.len();
+                let mut row: Vec<i32> = Vec::new();
+                for i in 0..=len {
+                    let v = if i == 0 {
+                        last[i]
+                    } else if i == len {
+                        last[i - 1]
+                    } else {
+                        last[i - 1] + last[i]
+                    };
+                    row.push(v);
+                }
+                rows.push(row);
+            }
+        }
+
+        rows
     }
 }
 
