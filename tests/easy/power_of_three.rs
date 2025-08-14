c37118d16952d67527f8fc81807b620284f1e221
Bruce Tsai <bruce.tsai@opennet.tw>
2025-08-14 00:51:38 +0000

    Solve problem 2787

--- tests/easy/power_of_three.rs
+++ tests/easy/power_of_three.rs
@@ -5,7 +5,20 @@ struct Solution;
 
 impl Solution {
     pub fn is_power_of_three(n: i32) -> bool {
-        todo!()
+        let mut n = n;
+
+        if n < 1 {
+            false
+        } else {
+            while n > 1 {
+                if n % 3 == 0 {
+                    n = n / 3;
+                } else {
+                    return false;
+                }
+            }
+            n == 1
+        }
     }
 }
 
@@ -16,18 +29,18 @@ mod tests {
     #[test]
     fn test_is_power_of_three_1() {
         let n = 27;
-        assert!(Solution::is_power_of_three(n));
+        assert_eq!(true, Solution::is_power_of_three(n));
     }
 
     #[test]
     fn test_is_power_of_three_2() {
         let n = 0;
-        assert!(Solution::is_power_of_three(n));
+        assert_eq!(false, Solution::is_power_of_three(n));
     }
 
     #[test]
     fn test_is_power_of_three_3() {
         let n = -1;
-        assert!(Solution::is_power_of_three(n));
+        assert_eq!(false, Solution::is_power_of_three(n));
     }
 }
