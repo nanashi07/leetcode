74963f02d6ad53aef03c0711883a82a5afe0c8f7
Bruce Tsai <bruce.tsai@opennet.tw>
2025-08-22 12:08:48 +0000

    Solve problem 3195

--- tests/medium/find_the_minimum_area_to_cover_all_ones_i.rs
+++ tests/medium/find_the_minimum_area_to_cover_all_ones_i.rs
@@ -5,7 +5,32 @@ struct Solution;
 
 impl Solution {
     pub fn minimum_area(grid: Vec<Vec<i32>>) -> i32 {
-        todo!()
+        println!("grid: {:?}", &grid);
+
+        let w = grid.len();
+        let h = grid[0].len();
+
+        let mut left = 0;
+        let mut right = grid.len() - 1;
+        let mut top = 0;
+        let mut bottom = grid[0].len() - 1;
+
+        while left < w && (0..h).fold(0, |a, c| a + grid[left][c]) == 0 {
+            left += 1;
+        }
+        while right > 0 && (0..h).fold(0, |a, c| a + grid[right][c]) == 0 {
+            right -= 1;
+        }
+        while top < h && (0..w).fold(0, |a, c| a + grid[c][top]) == 0 {
+            top += 1;
+        }
+        while bottom > 0 && (0..w).fold(0, |a, c| a + grid[c][bottom]) == 0 {
+            bottom -= 1;
+        }
+
+        println!("left: {left}, right: {right}, top: {top}, bottom: {bottom}");
+
+        ((right - left + 1) * (bottom - top + 1)) as i32
     }
 }
 
