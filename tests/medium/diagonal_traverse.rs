9b1f63e7b31c9631d784f3abe416e6d823c40ccc
Bruce Tsai <bruce.tsai@opennet.tw>
2025-08-25 13:03:37 +0000

    Problem 498

--- tests/medium/diagonal_traverse.rs
+++ tests/medium/diagonal_traverse.rs
@@ -5,7 +5,92 @@ struct Solution;
 
 impl Solution {
     pub fn find_diagonal_order(mat: Vec<Vec<i32>>) -> Vec<i32> {
-        todo!()
+        let m = mat.len();
+        let n = mat[0].len();
+        let mut result = Vec::new();
+
+        let mut row = 0;
+        let mut col = 0;
+        let mut going_up = true;
+
+        for _ in 0..(m * n) {
+            result.push(mat[row][col]);
+
+            if going_up {
+                // Moving diagonally up-right
+                if col == n - 1 {
+                    // Hit right boundary, move down and change direction
+                    row += 1;
+                    going_up = false;
+                } else if row == 0 {
+                    // Hit top boundary, move right and change direction
+                    col += 1;
+                    going_up = false;
+                } else {
+                    // Normal diagonal movement up-right
+                    row -= 1;
+                    col += 1;
+                }
+            } else {
+                // Moving diagonally down-left
+                if row == m - 1 {
+                    // Hit bottom boundary, move right and change direction
+                    col += 1;
+                    going_up = true;
+                } else if col == 0 {
+                    // Hit left boundary, move down and change direction
+                    row += 1;
+                    going_up = true;
+                } else {
+                    // Normal diagonal movement down-left
+                    row += 1;
+                    col -= 1;
+                }
+            }
+        }
+
+        result
+    }
+
+    // incorrect
+    pub fn _find_diagonal_order(mat: Vec<Vec<i32>>) -> Vec<i32> {
+        println!("mat: {:?}", &mat);
+
+        let m = mat.len() as i32;
+        let n = mat[0].len() as i32;
+
+        let mut i: i32 = 0;
+        let mut j: i32 = 0;
+        let mut r = vec![];
+
+        for s in 0..=(m - 1 + n - 1) {
+            println!("[{}, {}]", i, j);
+            let mv = if i <= j { -1 } else { 1 };
+            for x in 0..=s {
+                if i - mv * x < m && j + mv * x < n {
+                    // println!("{:?}", (i - mv * x, j + mv * x));
+                    r.push(mat[(i - mv * x) as usize][(j + mv * x) as usize]);
+                }
+            }
+
+            if j <= i {
+                i += 1;
+                if i >= m {
+                    i = m - 1;
+                    j += 1;
+                }
+            } else {
+                j += 1;
+                if j >= n {
+                    j = n - 1;
+                    i += 1;
+                }
+            }
+
+            (i, j) = (j, i);
+        }
+
+        r
     }
 }
 
@@ -32,5 +117,26 @@ mod tests {
             .map(|l| l.to_vec())
             .collect::<Vec<Vec<i32>>>();
         assert_eq!([1, 2, 3, 4].to_vec(), Solution::find_diagonal_order(mat));
+    }
+
+    #[test]
+    fn test_find_diagonal_order_3() {
+        let mat = [[6, 9, 7]]
+            .into_iter()
+            .map(|l| l.to_vec())
+            .collect::<Vec<Vec<i32>>>();
+        assert_eq!([6, 9, 7].to_vec(), Solution::find_diagonal_order(mat));
+    }
+
+    #[test]
+    fn test_find_diagonal_order_4() {
+        let mat = [[2, 5, 8], [4, 0, -1]]
+            .into_iter()
+            .map(|l| l.to_vec())
+            .collect::<Vec<Vec<i32>>>();
+        assert_eq!(
+            [2, 5, 4, 0, 8, -1].to_vec(),
+            Solution::find_diagonal_order(mat)
+        );
     }
 }
