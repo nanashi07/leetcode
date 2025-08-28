c298d28e97cb4ce5d553bf3532f8bc0b76775406
Bruce Tsai <bruce.tsai@opennet.tw>
2025-08-28 11:31:18 +0000

    Solve problem 3459

--- tests/medium/sort_matrix_by_diagonals.rs
+++ tests/medium/sort_matrix_by_diagonals.rs
@@ -5,7 +5,47 @@ struct Solution;
 
 impl Solution {
     pub fn sort_matrix(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        todo!()
+        grid.iter().for_each(|l| println!("{:?}", l));
+        let mut grid = grid;
+
+        let l = grid.len();
+        let mut i = 0;
+        let mut j = l - 1;
+
+        while i < l && j < l {
+            let mut bubble = false;
+            for m in 0..l {
+                let ci = i + m;
+                let cj = j + m;
+                let ni = i + m + 1;
+                let nj = j + m + 1;
+
+                if ni < l && nj < l {
+                    println!("move {:?} / {:?}", (ni, nj), (i, j));
+
+                    if i == 0 && grid[cj][ci] < grid[nj][ni] {
+                        (grid[cj][ci], grid[nj][ni]) = (grid[nj][ni], grid[cj][ci]);
+                        bubble = true;
+                    } else if i > 0 && grid[cj][ci] > grid[nj][ni] {
+                        (grid[cj][ci], grid[nj][ni]) = (grid[nj][ni], grid[cj][ci]);
+                        bubble = true;
+                    }
+                } else {
+                    if !bubble {
+                        if i == 0 && j > 0 {
+                            j -= 1;
+                        } else {
+                            i += 1;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        grid.iter().for_each(|l| println!("{:?}", l));
+
+        grid
     }
 }
 
@@ -46,6 +86,19 @@ mod tests {
             .map(|l| l.to_vec())
             .collect::<Vec<Vec<i32>>>();
         let output = [[1]]
+            .into_iter()
+            .map(|l| l.to_vec())
+            .collect::<Vec<Vec<i32>>>();
+        assert_eq!(output, Solution::sort_matrix(grid));
+    }
+
+    #[test]
+    fn test_sort_matrix_4() {
+        let grid = [[-1, -2, -3], [-3, -3, -2], [-4, -4, 0]]
+            .into_iter()
+            .map(|l| l.to_vec())
+            .collect::<Vec<Vec<i32>>>();
+        let output = [[0, -2, -3], [-3, -1, -2], [-4, -4, -3]]
             .into_iter()
             .map(|l| l.to_vec())
             .collect::<Vec<Vec<i32>>>();
