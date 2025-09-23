d46cee81433722c6d2649d8b8e91587300cf8277
Bruce Tsai <bruce.tsai@opennet.tw>
2025-09-23 12:39:34 +0000

    Solve problem 165

--- tests/medium/compare_version_numbers.rs
+++ tests/medium/compare_version_numbers.rs
@@ -5,7 +5,32 @@ struct Solution;
 
 impl Solution {
     pub fn compare_version(version1: String, version2: String) -> i32 {
-        todo!()
+        println!("version!: {:?}, version2: {:?}", &version1, &version2);
+
+        let v1 = version1
+            .split(".")
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+        let v2 = version2
+            .split(".")
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        let len = v1.len().max(v2.len());
+
+        println!("v1: {:?}, v2: {:?}", &v1, &v2);
+
+        for i in 0..len {
+            let n1 = v1.get(i).unwrap_or(&0);
+            let n2 = v2.get(i).unwrap_or(&0);
+            if n1 > n2 {
+                return 1;
+            } else if n1 < n2 {
+                return -1;
+            }
+        }
+
+        0
     }
 }
 
@@ -25,5 +50,26 @@ mod tests {
         let version1 = "1.01".to_string();
         let version2 = "1.001".to_string();
         assert_eq!(0, Solution::compare_version(version1, version2));
+    }
+
+    #[test]
+    fn test_compare_version_3() {
+        let version1 = "1.0".to_string();
+        let version2 = "1.0.0.0".to_string();
+        assert_eq!(0, Solution::compare_version(version1, version2));
+    }
+
+    #[test]
+    fn test_compare_version_4() {
+        let version1 = "1.0.1".to_string();
+        let version2 = "1".to_string();
+        assert_eq!(1, Solution::compare_version(version1, version2));
+    }
+
+    #[test]
+    fn test_compare_version_5() {
+        let version1 = "1".to_string();
+        let version2 = "1.1".to_string();
+        assert_eq!(-1, Solution::compare_version(version1, version2));
     }
 }
