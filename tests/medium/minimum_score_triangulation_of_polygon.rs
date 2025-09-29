f998ddb9cce6c041485b9fa02d37a6666f7d9289
Bruce Tsai <bruce.tsai@opennet.tw>
2025-09-29 09:24:38 +0000

    Problem 1039

--- tests/medium/minimum_score_triangulation_of_polygon.rs
+++ tests/medium/minimum_score_triangulation_of_polygon.rs
@@ -5,7 +5,29 @@ struct Solution;
 
 impl Solution {
     pub fn min_score_triangulation(values: Vec<i32>) -> i32 {
-        todo!()
+        let n = values.len();
+        // dp[i][j] represents the minimum score to triangulate polygon from vertex i to j
+        let mut dp = vec![vec![0; n]; n];
+
+        // len is the length of the chain (number of vertices in the sub-polygon)
+        // We start from length 3 (minimum for a triangle)
+        for len in 3..=n {
+            // i is the starting vertex
+            for i in 0..=n - len {
+                let j = i + len - 1; // ending vertex
+                dp[i][j] = i32::MAX;
+
+                // Try all possible middle vertices k to form triangle (i, k, j)
+                for k in i + 1..j {
+                    // Score of triangle (i, k, j) + score of left sub-polygon + score of right sub-polygon
+                    let score = values[i] * values[k] * values[j] + dp[i][k] + dp[k][j];
+                    dp[i][j] = dp[i][j].min(score);
+                }
+            }
+        }
+
+        // Return the minimum score for the entire polygon (from vertex 0 to n-1)
+        dp[0][n - 1]
     }
 }
 
