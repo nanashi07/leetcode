5030f8a27f6027ae75afc25b4b73451ad1f69d7e
Bruce Tsai <bruce.tsai@opennet.tw>
2025-10-09 13:46:39 +0000

    Problem 3494

--- tests/medium/find_the_minimum_amount_of_time_to_brew_potions.rs
+++ tests/medium/find_the_minimum_amount_of_time_to_brew_potions.rs
@@ -5,7 +5,21 @@ struct Solution;
 
 impl Solution {
     pub fn min_time(skill: Vec<i32>, mana: Vec<i32>) -> i64 {
-        todo!()
+        let n = skill.len();
+        let mut times = vec![0i64; n];
+
+        for &mj in &mana {
+            let mut cur_time: i64 = 0;
+            for i in 0..n {
+                cur_time = std::cmp::max(cur_time, times[i]) + skill[i] as i64 * mj as i64;
+            }
+            times[n - 1] = cur_time;
+            for i in (0..n - 1).rev() {
+                times[i] = times[i + 1] - skill[i + 1] as i64 * mj as i64;
+            }
+        }
+
+        times[n - 1]
     }
 }
 
