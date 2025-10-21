21d636a8f0ace0d8d6b35140d9cffe18a6e3fecc
Bruce Tsai <bruce.tsai@opennet.tw>
2025-10-21 00:18:58 +0000

    Problem 2011

--- tests/easy/final_value_of_variable_after_performing_operations.rs
+++ tests/easy/final_value_of_variable_after_performing_operations.rs
@@ -5,7 +5,21 @@ struct Solution;
 
 impl Solution {
     pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
-        todo!()
+        println!("operations: {:?}", &operations);
+
+        let mut x = 0;
+
+        for operation in operations {
+            match operation.as_str() {
+                "++X" => x += 1,
+                "X++" => x += 1,
+                "--X" => x -= 1,
+                "X--" => x -= 1,
+                _ => {}
+            }
+        }
+
+        x
     }
 }
 
