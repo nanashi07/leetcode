1ccffee116321ece56c01113fc60a0dc1ff3acab
Bruce Tsai <bruce.tsai@opennet.tw>
2025-10-29 00:21:13 +0000

    Problem 3370

---
+++ tests/easy/smallest_number_with_all_set_bits.rs
@@ -0,0 +1,33 @@
+// # 3370. Smallest Number With All Set Bits
+// https://leetcode.com/problems/smallest-number-with-all-set-bits/
+
+struct Solution;
+
+impl Solution {
+    pub fn smallest_number(n: i32) -> i32 {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::easy::smallest_number_with_all_set_bits::Solution;
+
+    #[test]
+    fn test_smallest_number_1() {
+        let n = 5;
+        assert_eq!(7, Solution::smallest_number(n));
+    }
+
+    #[test]
+    fn test_smallest_number_2() {
+        let n = 10;
+        assert_eq!(15, Solution::smallest_number(n));
+    }
+
+    #[test]
+    fn test_smallest_number_3() {
+        let n = 3;
+        assert_eq!(3, Solution::smallest_number(n));
+    }
+}
