fbbcc348d1f466547f9eca3394392b09d852d417
Bruce Tsai <bruce.tsai@opennet.tw>
2025-11-14 00:30:46 +0000

    Problem 2536

--- tests/medium/increment_submatrices_by_one.rs
+++ tests/medium/increment_submatrices_by_one.rs
@@ -5,7 +5,42 @@ struct Solution;
 
 impl Solution {
     pub fn range_add_queries(n: i32, queries: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        todo!()
+        let n = n as usize;
+        let mut diff = vec![vec![0; n + 1]; n + 1];
+
+        // Apply difference array technique
+        for query in queries {
+            let (r1, c1, r2, c2) = (
+                query[0] as usize,
+                query[1] as usize,
+                query[2] as usize,
+                query[3] as usize,
+            );
+
+            diff[r1][c1] += 1;
+            diff[r1][c2 + 1] -= 1;
+            diff[r2 + 1][c1] -= 1;
+            diff[r2 + 1][c2 + 1] += 1;
+        }
+
+        // Compute 2D prefix sum to get the result
+        let mut result = vec![vec![0; n]; n];
+        for i in 0..n {
+            for j in 0..n {
+                result[i][j] = diff[i][j];
+                if i > 0 {
+                    result[i][j] += result[i - 1][j];
+                }
+                if j > 0 {
+                    result[i][j] += result[i][j - 1];
+                }
+                if i > 0 && j > 0 {
+                    result[i][j] -= result[i - 1][j - 1];
+                }
+            }
+        }
+
+        result
     }
 }
 
