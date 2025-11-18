864f3e963938a0df83d7bfa0ff62ca69402adff9
Bruce Tsai <bruce.tsai@opennet.tw>
2025-11-18 01:08:58 +0000

    Problem 717

--- tests/easy/one_bit_and_two_bit_characters.rs
+++ tests/easy/one_bit_and_two_bit_characters.rs
@@ -5,7 +5,29 @@ struct Solution;
 
 impl Solution {
     pub fn is_one_bit_character(bits: Vec<i32>) -> bool {
-        todo!()
+        println!("bits: {:?}", &bits);
+
+        Self::extract(&bits[..bits.len() - 1])
+    }
+
+    fn extract(bits: &[i32]) -> bool {
+        let len = bits.len();
+        match len {
+            0 => true,
+            1 => &bits == &[0],
+            _ => {
+                if &bits[len - 1] == &0 && Self::extract(&bits[..len - 1]) {
+                    return true;
+                }
+                if &bits[len - 2..] == &[1, 0] && Self::extract(&bits[..len - 2]) {
+                    return true;
+                }
+                if &bits[len - 2..] == &[1, 1] && Self::extract(&bits[..len - 2]) {
+                    return true;
+                }
+                return false;
+            }
+        }
     }
 }
 
