3ddcca98b1e65e390e4e26d912e4e03ff950b0d8
Bruce Tsai <bruce.tsai@opennet.tw>
2025-11-22 00:26:04 +0000

    Problem 3190

--- tests/easy/find_minimum_operations_to_make_all_elements_divisible_by_three.rs
+++ tests/easy/find_minimum_operations_to_make_all_elements_divisible_by_three.rs
@@ -5,7 +5,9 @@ struct Solution;
 
 impl Solution {
     pub fn minimum_operations(nums: Vec<i32>) -> i32 {
-        todo!()
+        println!("nums: {:?}", &nums);
+
+        nums.iter().filter(|&n| n % 3 != 0).count() as i32
     }
 }
 
