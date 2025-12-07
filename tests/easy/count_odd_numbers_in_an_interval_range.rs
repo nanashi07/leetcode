4c0e6910d1c4ed69b52b86c259fb8924808116e2
Bruce Tsai <bruce.tsai@opennet.tw>
2025-12-07 02:14:05 +0000

    Problem 1523

--- tests/easy/count_odd_numbers_in_an_interval_range.rs
+++ tests/easy/count_odd_numbers_in_an_interval_range.rs
@@ -5,7 +5,12 @@ struct Solution;
 
 impl Solution {
     pub fn count_odds(low: i32, high: i32) -> i32 {
-        todo!()
+        println!("low: {low}, high: {high}");
+
+        let low = ((low as f64 - 1.0) / 2.0).ceil();
+        let high = ((high as f64 - 1.0) / 2.0).floor();
+
+        1 + (high - low) as i32
     }
 }
 
@@ -25,5 +30,19 @@ mod tests {
         let low = 8;
         let high = 10;
         assert_eq!(1, Solution::count_odds(low, high));
+    }
+
+    #[test]
+    fn test_count_odds_3() {
+        let low = 0;
+        let high = 10;
+        assert_eq!(5, Solution::count_odds(low, high));
+    }
+
+    #[test]
+    fn test_count_odds_4() {
+        let low = 800445804;
+        let high = 979430543;
+        assert_eq!(89492370, Solution::count_odds(low, high));
     }
 }
