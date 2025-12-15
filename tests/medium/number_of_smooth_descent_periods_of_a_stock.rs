bc6e0f01b23e46fa3394b279f8356907ba5be81e
Bruce Tsai <bruce.tsai@opennet.tw>
2025-12-15 02:29:48 +0000

    Problem 2110

--- tests/medium/number_of_smooth_descent_periods_of_a_stock.rs
+++ tests/medium/number_of_smooth_descent_periods_of_a_stock.rs
@@ -5,7 +5,29 @@ struct Solution;
 
 impl Solution {
     pub fn get_descent_periods(prices: Vec<i32>) -> i64 {
-        todo!()
+        println!("prices: {:?}", &prices);
+
+        let mut start = 0;
+        let mut sec = vec![];
+
+        for i in 1..prices.len() {
+            let p = prices[i];
+            if p == prices[i - 1] - 1 {
+                // go next
+                if i == prices.len() - 1 && i - start > 0 {
+                    sec.push((i - start + 1) as i64);
+                }
+            } else {
+                if i - start > 1 {
+                    sec.push((i - start) as i64);
+                }
+                start = i;
+            }
+        }
+
+        println!("sec: {:?}", &sec);
+
+        sec.iter().map(|s| (s + 1) * s / 2 - s).sum::<i64>() + prices.len() as i64
     }
 }
 
@@ -29,5 +51,11 @@ mod tests {
     fn test_get_descent_periods_3() {
         let prices = [1].to_vec();
         assert_eq!(1, Solution::get_descent_periods(prices));
+    }
+
+    #[test]
+    fn test_get_descent_periods_4() {
+        let prices = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 4, 3, 10, 9, 8, 7].to_vec();
+        assert_eq!(68, Solution::get_descent_periods(prices));
     }
 }
