676bb0d7f4f0f6ee5512731efc470247d77dd0aa
Bruce Tsai <bruce.tsai@opennet.tw>
2025-12-19 00:20:43 +0000

    Problem 2092

--- tests/hard/find_all_people_with_secret.rs
+++ tests/hard/find_all_people_with_secret.rs
@@ -1,11 +1,84 @@
 // 2092. Find All People With Secret
 // https://leetcode.com/problems/find-all-people-with-secret/
 
+use std::collections::{HashMap, HashSet};
+
 struct Solution;
 
 impl Solution {
-    pub fn find_all_people(n: i32, meetings: Vec<Vec<i32>>, first_person: i32) -> Vec<i32> {
-        todo!()
+    pub fn find_all_people(_n: i32, meetings: Vec<Vec<i32>>, first_person: i32) -> Vec<i32> {
+        // Group meetings by time
+        let mut meetings_by_time: HashMap<i32, Vec<(i32, i32)>> = HashMap::new();
+        for meeting in meetings.iter() {
+            let (person1, person2, time) = (meeting[0], meeting[1], meeting[2]);
+            meetings_by_time
+                .entry(time)
+                .or_insert(Vec::new())
+                .push((person1, person2));
+        }
+
+        // Sort times
+        let mut times: Vec<i32> = meetings_by_time.keys().copied().collect();
+        times.sort_unstable();
+
+        // Track who knows the secret
+        let mut knows_secret: HashSet<i32> = HashSet::new();
+        knows_secret.insert(0);
+        knows_secret.insert(first_person);
+
+        // Process meetings at each time
+        for time in times {
+            let meetings_at_time = meetings_by_time.get(&time).unwrap();
+
+            // Build graph for this time slot
+            let mut graph: HashMap<i32, Vec<i32>> = HashMap::new();
+            let mut people_at_meeting: HashSet<i32> = HashSet::new();
+
+            for &(person1, person2) in meetings_at_time {
+                graph.entry(person1).or_insert(Vec::new()).push(person2);
+                graph.entry(person2).or_insert(Vec::new()).push(person1);
+                people_at_meeting.insert(person1);
+                people_at_meeting.insert(person2);
+            }
+
+            // Find all people who know secret at this time (before meetings)
+            let initial_knows: Vec<i32> = people_at_meeting
+                .iter()
+                .filter(|&&p| knows_secret.contains(&p))
+                .copied()
+                .collect();
+
+            // BFS/DFS to spread secret through connected components
+            let mut visited: HashSet<i32> = HashSet::new();
+
+            for start in initial_knows {
+                if visited.contains(&start) {
+                    continue;
+                }
+
+                // BFS from this person
+                let mut queue: Vec<i32> = vec![start];
+                visited.insert(start);
+
+                while let Some(person) = queue.pop() {
+                    knows_secret.insert(person);
+
+                    if let Some(neighbors) = graph.get(&person) {
+                        for &neighbor in neighbors {
+                            if !visited.contains(&neighbor) {
+                                visited.insert(neighbor);
+                                queue.push(neighbor);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Collect and sort result
+        let mut result: Vec<i32> = knows_secret.into_iter().collect();
+        result.sort_unstable();
+        result
     }
 }
 
