586cd6b215830a2797abb52c8b77712fe47f4236
Bruce Tsai <bruce.tsai@opennet.tw>
2025-12-24 00:16:23 +0000

    Problem 3074

--- tests/easy/apple_redistribution_into_boxes.rs
+++ tests/easy/apple_redistribution_into_boxes.rs
@@ -5,7 +5,20 @@ struct Solution;
 
 impl Solution {
     pub fn minimum_boxes(apple: Vec<i32>, capacity: Vec<i32>) -> i32 {
-        todo!()
+        println!("apple: {:?}, capacity: {:?}", &apple, &capacity);
+
+        let mut sum_of_apple = apple.iter().sum::<i32>();
+        let mut capacity = capacity;
+        capacity.sort_unstable();
+
+        for i in (0..capacity.len()).rev() {
+            sum_of_apple -= capacity[i];
+            if sum_of_apple <= 0 {
+                return (capacity.len() - i) as i32;
+            }
+        }
+
+        capacity.len() as i32
     }
 }
 
