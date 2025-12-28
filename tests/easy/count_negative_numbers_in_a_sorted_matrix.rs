38b32c268758b0c7ecfa24126dd19f211e1c6533
Bruce Tsai <bruce.tsai@opennet.tw>
2025-12-28 00:35:24 +0000

    Problem 1351

--- tests/easy/count_negative_numbers_in_a_sorted_matrix.rs
+++ tests/easy/count_negative_numbers_in_a_sorted_matrix.rs
@@ -5,7 +5,20 @@ struct Solution;
 
 impl Solution {
     pub fn count_negatives(grid: Vec<Vec<i32>>) -> i32 {
-        todo!()
+        println!("grid: {:?}", &grid);
+
+        let mut c = 0;
+
+        for i in 0..grid.len() {
+            for j in (0..grid[i].len()).rev() {
+                if grid[i][j] >= 0 {
+                    break;
+                }
+                c += 1;
+            }
+        }
+
+        c
     }
 }
 
@@ -33,6 +46,6 @@ mod tests {
             .iter()
             .map(|l| l.to_vec())
             .collect::<Vec<_>>();
-        assert_eq!(8, Solution::count_negatives(grid));
+        assert_eq!(0, Solution::count_negatives(grid));
     }
 }
