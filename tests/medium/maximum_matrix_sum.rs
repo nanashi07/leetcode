b107fde4fcb670cc9e7371496d7dba5982e67d76
Bruce Tsai <bruce.tsai@opennet.tw>
2026-01-05 03:14:23 +0000

    Problem 1975

--- tests/medium/maximum_matrix_sum.rs
+++ tests/medium/maximum_matrix_sum.rs
@@ -5,7 +5,29 @@ struct Solution;
 
 impl Solution {
     pub fn max_matrix_sum(matrix: Vec<Vec<i32>>) -> i64 {
-        todo!()
+        let mut total_sum: i64 = 0;
+        let mut min_abs_value = i32::MAX;
+        let mut negative_count = 0;
+
+        for row in &matrix {
+            for &val in row {
+                let abs_val = val.abs();
+                total_sum += abs_val as i64;
+                min_abs_value = min_abs_value.min(abs_val);
+
+                if val < 0 {
+                    negative_count += 1;
+                }
+            }
+        }
+
+        // If we have an odd number of negatives, we must keep one negative
+        // The best strategy is to make the smallest absolute value negative
+        if negative_count % 2 == 1 {
+            total_sum - 2 * min_abs_value as i64
+        } else {
+            total_sum
+        }
     }
 }
 
