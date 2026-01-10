f5bad055a5b945b3d2ec03a165133bf117a42da5
Bruce Tsai <bruce.tsai@opennet.tw>
2026-01-10 01:00:05 +0000

    Problem 712

--- tests/medium/minimum_ascii_delete_sum_for_two_strings.rs
+++ tests/medium/minimum_ascii_delete_sum_for_two_strings.rs
@@ -5,7 +5,41 @@ struct Solution;
 
 impl Solution {
     pub fn minimum_delete_sum(s1: String, s2: String) -> i32 {
-        todo!()
+        let s1_bytes = s1.as_bytes();
+        let s2_bytes = s2.as_bytes();
+        let m = s1_bytes.len();
+        let n = s2_bytes.len();
+
+        // dp[i][j] represents the minimum delete sum to make s1[0..i] and s2[0..j] equal
+        let mut dp = vec![vec![0; n + 1]; m + 1];
+
+        // Base case: if s2 is empty, delete all characters from s1
+        for i in 1..=m {
+            dp[i][0] = dp[i - 1][0] + s1_bytes[i - 1] as i32;
+        }
+
+        // Base case: if s1 is empty, delete all characters from s2
+        for j in 1..=n {
+            dp[0][j] = dp[0][j - 1] + s2_bytes[j - 1] as i32;
+        }
+
+        // Fill the dp table
+        for i in 1..=m {
+            for j in 1..=n {
+                if s1_bytes[i - 1] == s2_bytes[j - 1] {
+                    // Characters match, no deletion needed
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    // Characters don't match, delete from either s1 or s2
+                    dp[i][j] = std::cmp::min(
+                        dp[i - 1][j] + s1_bytes[i - 1] as i32, // Delete from s1
+                        dp[i][j - 1] + s2_bytes[j - 1] as i32, // Delete from s2
+                    );
+                }
+            }
+        }
+
+        dp[m][n]
     }
 }
 
