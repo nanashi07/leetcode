405cf65182eff98dc619eb7b41240dbcd1386da4
Bruce Tsai <bruce.tsai@opennet.tw>
2026-01-21 00:21:29 +0000

    Problem 3315

--- tests/medium/construct_the_minimum_bitwise_array_ii.rs
+++ tests/medium/construct_the_minimum_bitwise_array_ii.rs
@@ -5,7 +5,37 @@ struct Solution;
 
 impl Solution {
     pub fn min_bitwise_array(nums: Vec<i32>) -> Vec<i32> {
-        todo!()
+        nums.into_iter()
+            .map(|num| {
+                // If num is even, no solution exists
+                if num % 2 == 0 {
+                    return -1;
+                }
+
+                // For odd numbers, we need to find ans such that ans | (ans + 1) = num
+                // Key insight: ans | (ans + 1) always sets all bits from the rightmost 0 in ans to the right
+                // We need to find the rightmost position where we can clear a bit in num
+
+                // Find the rightmost consecutive 1s and flip the rightmost one
+                // Example: 7 = 111, we want 011 = 3, then 3 | 4 = 011 | 100 = 111 = 7
+                // Example: 5 = 101, we want 100 = 4, then 4 | 5 = 100 | 101 = 101 = 5
+                // Example: 11 = 1011, we want 1001 = 9, then 9 | 10 = 1001 | 1010 = 1011 = 11
+
+                // Find the first position from right where bit transitions from 1 to 0
+                let mut ans = num;
+                let mut pos = 0;
+
+                // Find rightmost consecutive 1s
+                while (ans >> pos) & 1 == 1 {
+                    pos += 1;
+                }
+
+                // Clear the bit at position (pos - 1)
+                ans &= !(1 << (pos - 1));
+
+                ans
+            })
+            .collect()
     }
 }
 
