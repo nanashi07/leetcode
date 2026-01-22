3baeda8b663851a8147b0c0294b95f161e3d7006
Bruce Tsai <bruce.tsai@opennet.tw>
2026-01-22 01:21:53 +0000

    Problem 3507

--- tests/easy/minimum_pair_removal_to_sort_array_i.rs
+++ tests/easy/minimum_pair_removal_to_sort_array_i.rs
@@ -5,7 +5,37 @@ struct Solution;
 
 impl Solution {
     pub fn minimum_pair_removal(nums: Vec<i32>) -> i32 {
-        todo!()
+        println!("nums: {nums:?}");
+
+        let mut count = 0;
+        let mut nums = nums;
+
+        while !Self::is_sorted(&nums) {
+            let sums = &nums
+                .iter()
+                .take(nums.len() - 1)
+                .enumerate()
+                .map(|(i, n)| *n + nums[i + 1])
+                .collect::<Vec<_>>();
+            let min = sums.iter().min().unwrap();
+            for i in 0..sums.len() {
+                if *min == sums[i] {
+                    let c = nums.clone();
+                    nums.remove(i + 1);
+                    nums[i] = *min;
+                    count += 1;
+                    break;
+                }
+            }
+        }
+
+        count
+    }
+
+    fn is_sorted(nums: &Vec<i32>) -> bool {
+        let mut sorted = nums.clone();
+        sorted.sort_unstable();
+        sorted.eq(nums)
     }
 }
 
@@ -23,5 +53,11 @@ mod tests {
     fn test_minimum_pair_removal_2() {
         let nums = [1, 2, 2].to_vec();
         assert_eq!(0, Solution::minimum_pair_removal(nums));
+    }
+
+    #[test]
+    fn test_minimum_pair_removal_3() {
+        let nums = [-2, 1, 2, -1, -1, -2, -2, -1, -1, 1, 1].to_vec();
+        assert_eq!(10, Solution::minimum_pair_removal(nums));
     }
 }
