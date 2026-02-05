e3e568dde8fb667c52d66f102ddea6559a54aa44
Bruce Tsai <bruce.tsai@opennet.tw>
2026-02-05 01:32:46 +0000

    Problem 3379

--- tests/easy/transformed_array.rs
+++ tests/easy/transformed_array.rs
@@ -5,7 +5,22 @@ struct Solution;
 
 impl Solution {
     pub fn construct_transformed_array(nums: Vec<i32>) -> Vec<i32> {
-        todo!()
+        println!("nums: {nums:?}");
+
+        let len = nums.len() as i32;
+        let mut results = vec![0; len as usize];
+
+        for i in 0..len {
+            let n = &nums[i as usize];
+            let mut n = i + *n;
+            if n < 0 {
+                n = n + (n.abs() as f64 / len as f64).ceil() as i32 * len;
+            }
+            n = n % len;
+            results[i as usize] = nums[n as usize];
+        }
+
+        results
     }
 }
 
@@ -24,6 +39,13 @@ mod tests {
     fn test_construct_transformed_array_2() {
         let nums = [-1, 4, -1].to_vec();
         let output = [-1, -1, 4].to_vec();
+        assert_eq!(output, Solution::construct_transformed_array(nums));
+    }
+
+    #[test]
+    fn test_construct_transformed_array_3() {
+        let nums = [-10].to_vec();
+        let output = [-10].to_vec();
         assert_eq!(output, Solution::construct_transformed_array(nums));
     }
 }
