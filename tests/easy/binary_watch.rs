44a4e3976ad8275c124e9a76a6c8a08115e03a65
Bruce Tsai <bruce.tsai@opennet.tw>
2026-02-17 11:53:52 +0000

    Problem 401

--- tests/easy/binary_watch.rs
+++ tests/easy/binary_watch.rs
@@ -5,7 +5,22 @@ struct Solution;
 
 impl Solution {
     pub fn read_binary_watch(turned_on: i32) -> Vec<String> {
-        todo!()
+        let mut result = Vec::new();
+
+        // Iterate through all valid hours (0-11) and minutes (0-59)
+        for hour in 0u32..12 {
+            for minute in 0u32..60 {
+                // Count the number of 1s in the binary representation
+                let bits = hour.count_ones() + minute.count_ones();
+
+                if bits == turned_on as u32 {
+                    // Format as "H:MM"
+                    result.push(format!("{}:{:02}", hour, minute));
+                }
+            }
+        }
+
+        result
     }
 }
 
