63a64ab756caccf31b094376e8ff9a14bda582c8
Bruce Tsai <bruce.tsai@opennet.tw>
2026-02-22 01:01:11 +0000

    Problem 868

--- tests/easy/binary_gap.rs
+++ tests/easy/binary_gap.rs
@@ -5,7 +5,32 @@ struct Solution;
 
 impl Solution {
     pub fn binary_gap(n: i32) -> i32 {
-        todo!()
+        println!("n: {n} ({n:b})");
+
+        let mut n = n;
+
+        while n > 0 && n % 2 == 0 {
+            n = n >> 1;
+        }
+
+        let mut p = -1;
+        let mut last = 0;
+        let mut max = -1;
+
+        while n > 0 {
+            p += 1;
+            let len = if n % 2 == 1 {
+                let l = p - last;
+                last = p;
+                l
+            } else {
+                p - last
+            };
+            max = max.max(len);
+            n = n >> 1;
+        }
+
+        max
     }
 }
 
@@ -29,5 +54,11 @@ mod tests {
     fn test_binary_gap_3() {
         let n = 5;
         assert_eq!(2, Solution::binary_gap(n));
+    }
+
+    #[test]
+    fn test_binary_gap_4() {
+        let n = 6;
+        assert_eq!(1, Solution::binary_gap(n));
     }
 }
