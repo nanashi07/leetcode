651431caa7d83a7301a2ea4a405197a01ba07a61
Bruce Tsai <bruce.tsai@opennet.tw>
2026-02-26 00:21:19 +0000

    Problem 1404

--- tests/medium/mod.rs
+++ tests/medium/mod.rs
@@ -100,6 +100,8 @@ mod maximum_number_of_events_that_can_be_attended;
 mod balance_a_binary_search_tree;
 // 1390. Four Divisors
 mod four_divisors;
+// 1404. Number of Steps to Reduce a Number in Binary Representation to One
+pub mod number_of_steps_to_reduce_a_number_in_binary_representation_to_one;
 // 1461. Check If a String Contains All Binary Codes of Size K
 mod check_if_a_string_contains_all_binary_codes_of_size_k;
 // 1488. Avoid Flood in The City
