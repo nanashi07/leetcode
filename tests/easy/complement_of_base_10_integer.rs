a70ad1b5ba56d5990d20c216a4c5874045960f38
Bruce Tsai <bruce.tsai@opennet.tw>
2026-03-11 00:18:33 +0000

    Problem 1009

--- tests/easy/complement_of_base_10_integer.rs
+++ tests/easy/complement_of_base_10_integer.rs
@@ -5,7 +5,20 @@ struct Solution;
 
 impl Solution {
     pub fn bitwise_complement(n: i32) -> i32 {
-        todo!()
+        println!("n: {n}");
+
+        let mut n = n;
+        let mut r = 0;
+        let mut i = 0;
+
+        while i == 0 || n > 0 {
+            let c = (n % 2 - 1).abs();
+            r = r + (c << i);
+            n = n / 2;
+            i += 1;
+        }
+
+        r
     }
 }
 
@@ -29,5 +42,11 @@ mod tests {
     fn test_bitwise_complement_3() {
         let n = 10;
         assert_eq!(5, Solution::bitwise_complement(n));
+    }
+
+    #[test]
+    fn test_bitwise_complement_4() {
+        let n = 0;
+        assert_eq!(1, Solution::bitwise_complement(n));
     }
 }
