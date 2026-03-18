62ea9e690fe411c70aec04afef48d78c9ae56eb5
Bruce Tsai <bruce.tsai@opennet.tw>
2026-03-18 00:43:46 +0000

    Problem 3070

--- tests/medium/count_submatrices_with_top_left_element_and_sum_less_than_k.rs
+++ tests/medium/count_submatrices_with_top_left_element_and_sum_less_than_k.rs
@@ -5,7 +5,22 @@ struct Solution;
 
 impl Solution {
     pub fn count_submatrices(grid: Vec<Vec<i32>>, k: i32) -> i32 {
-        todo!()
+        let m = grid.len();
+        let n = grid[0].len();
+        let mut prefix = vec![vec![0i32; n]; m];
+        let mut count = 0;
+        for i in 0..m {
+            for j in 0..n {
+                prefix[i][j] = grid[i][j]
+                    + if i > 0 { prefix[i - 1][j] } else { 0 }
+                    + if j > 0 { prefix[i][j - 1] } else { 0 }
+                    - if i > 0 && j > 0 { prefix[i - 1][j - 1] } else { 0 };
+                if prefix[i][j] <= k {
+                    count += 1;
+                }
+            }
+        }
+        count
     }
 }
 
