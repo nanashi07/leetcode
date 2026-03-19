5ddbe7c73809f3a6727161f492bba9063463629b
Bruce Tsai <bruce.tsai@opennet.tw>
2026-03-19 00:16:00 +0000

    Problem 3212

--- tests/medium/count_submatrices_with_equal_frequency_of_x_and_y.rs
+++ tests/medium/count_submatrices_with_equal_frequency_of_x_and_y.rs
@@ -5,7 +5,24 @@ struct Solution;
 
 impl Solution {
     pub fn number_of_submatrices(grid: Vec<Vec<char>>) -> i32 {
-        todo!()
+        let m = grid.len();
+        let n = grid[0].len();
+        // prefix[i][j] = (count_x, count_y) for submatrix (0,0)-(i-1,j-1)
+        let mut px = vec![vec![0i32; n + 1]; m + 1];
+        let mut py = vec![vec![0i32; n + 1]; m + 1];
+        let mut count = 0;
+        for i in 1..=m {
+            for j in 1..=n {
+                let cx = if grid[i - 1][j - 1] == 'X' { 1 } else { 0 };
+                let cy = if grid[i - 1][j - 1] == 'Y' { 1 } else { 0 };
+                px[i][j] = px[i - 1][j] + px[i][j - 1] - px[i - 1][j - 1] + cx;
+                py[i][j] = py[i - 1][j] + py[i][j - 1] - py[i - 1][j - 1] + cy;
+                if px[i][j] > 0 && px[i][j] == py[i][j] {
+                    count += 1;
+                }
+            }
+        }
+        count
     }
 }
 
