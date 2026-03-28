114a510b9d53612c98f9703df52eafe486c24d52
Bruce Tsai <bruce.tsai@opennet.tw>
2026-03-28 00:08:36 +0000

    Problem 2573

--- tests/hard/mod.rs
+++ tests/hard/mod.rs
@@ -70,6 +70,8 @@ mod paths_in_matrix_whose_sum_is_divisible_by_k;
 mod maximize_the_minimum_powered_city;
 // 2561. Rearranging Fruits
 mod rearranging_fruits;
+// 2573. Find the String with LCP
+pub mod find_the_string_with_lcp;
 // 2872. Maximum Number of K-Divisible Components
 mod maximum_number_of_k_divisible_components;
 // 2977. Minimum Cost to Convert String II
