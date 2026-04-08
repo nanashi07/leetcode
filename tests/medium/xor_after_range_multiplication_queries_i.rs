7c79d81450ff5122572f3754c4cfc36c86873481
Bruce Tsai <bruce.tsai@opennet.tw>
2026-04-08 00:25:17 +0000

    Problem 3653

--- tests/medium/xor_after_range_multiplication_queries_i.rs
+++ tests/medium/xor_after_range_multiplication_queries_i.rs
@@ -4,8 +4,17 @@
 struct Solution;
 
 impl Solution {
-    pub fn xor_after_queries(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> i32 {
-        todo!()
+    pub fn xor_after_queries(mut nums: Vec<i32>, queries: Vec<Vec<i32>>) -> i32 {
+        const MOD: i64 = 1_000_000_007;
+        for q in &queries {
+            let (l, r, k, v) = (q[0] as usize, q[1] as usize, q[2] as usize, q[3] as i64);
+            let mut idx = l;
+            while idx <= r {
+                nums[idx] = ((nums[idx] as i64 * v) % MOD) as i32;
+                idx += k;
+            }
+        }
+        nums.iter().fold(0, |acc, &x| acc ^ x)
     }
 }
 
