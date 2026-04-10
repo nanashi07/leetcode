8505f11b6f72b672a916fb7ce85d9dd2c0716169
Bruce Tsai <bruce.tsai@opennet.tw>
2026-04-10 00:47:13 +0000

    Problem 3740

--- tests/easy/minimum_distance_between_three_equal_elements_i.rs
+++ tests/easy/minimum_distance_between_three_equal_elements_i.rs
@@ -14,28 +14,15 @@ impl Solution {
         let items = map
             .values()
             .filter(|values| values.len() >= 3)
-            .map(|values| {
-                values
-                    .iter()
-                    .enumerate()
-                    .map(|(i, p)| {
-                        if i == values.len() - 1 {
-                            *p - values[0]
-                        } else {
-                            values[i + 1] - *p
-                        }
-                    })
-                    .collect::<Vec<_>>()
-            })
             .collect::<Vec<_>>();
 
         let mut min: i32 = -1;
-        for mut item in items {
-            item.sort_unstable();
-            min = if min == -1 {
-                item.iter().take(3).sum::<usize>() as i32
-            } else {
-                min.min(item.iter().take(3).sum::<usize>() as i32)
+        for item in items {
+            for w in item.windows(3) {
+                let dist = 2 * (w[2] - w[0]) as i32;
+                if min == -1 || dist < min {
+                    min = dist;
+                }
             }
         }
 
@@ -63,5 +50,11 @@ mod tests {
     fn test_minimum_distance_3() {
         let nums = [1].to_vec();
         assert_eq!(-1, Solution::minimum_distance(nums));
+    }
+
+    #[test]
+    fn test_minimum_distance_4() {
+        let nums = [1, 1, 1, 1].to_vec();
+        assert_eq!(4, Solution::minimum_distance(nums));
     }
 }
