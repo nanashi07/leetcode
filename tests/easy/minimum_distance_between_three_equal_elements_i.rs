3809ed689457337b7f3fe16b6ae233afe08a9c23
Bruce Tsai <bruce.tsai@opennet.tw>
2026-04-10 00:39:22 +0000

    Problem 3740

--- tests/easy/minimum_distance_between_three_equal_elements_i.rs
+++ tests/easy/minimum_distance_between_three_equal_elements_i.rs
@@ -1,11 +1,45 @@
 // 3740. Minimum Distance Between Three Equal Elements I
 // https://leetcode.com/problems/minimum-distance-between-three-equal-elements-i/
 
+use std::collections::HashMap;
+
 struct Solution;
 
 impl Solution {
     pub fn minimum_distance(nums: Vec<i32>) -> i32 {
-        todo!()
+        let mut map: HashMap<i32, Vec<usize>> = HashMap::new();
+        for (i, n) in nums.iter().enumerate() {
+            map.entry(*n).and_modify(|c| c.push(i)).or_insert(vec![i]);
+        }
+        let items = map
+            .values()
+            .filter(|values| values.len() >= 3)
+            .map(|values| {
+                values
+                    .iter()
+                    .enumerate()
+                    .map(|(i, p)| {
+                        if i == values.len() - 1 {
+                            *p - values[0]
+                        } else {
+                            values[i + 1] - *p
+                        }
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        let mut min: i32 = -1;
+        for mut item in items {
+            item.sort_unstable();
+            min = if min == -1 {
+                item.iter().take(3).sum::<usize>() as i32
+            } else {
+                min.min(item.iter().take(3).sum::<usize>() as i32)
+            }
+        }
+
+        min
     }
 }
 
