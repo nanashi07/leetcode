ff1be76e8f4184c0ed518d58e2e7415906ae1b8e
Bruce Tsai <bruce.tsai@opennet.tw>
2026-04-13 00:31:27 +0000

    Problem 1848

--- tests/easy/minimum_distance_to_the_target_element.rs
+++ tests/easy/minimum_distance_to_the_target_element.rs
@@ -5,7 +5,13 @@ struct Solution;
 
 impl Solution {
     pub fn get_min_distance(nums: Vec<i32>, target: i32, start: i32) -> i32 {
-        todo!()
+        let mut min = i32::MAX;
+        for (i, n) in nums.iter().enumerate() {
+            if *n == target && min > (i as i32 - start).abs() {
+                min = (i as i32 - start).abs();
+            }
+        }
+        min
     }
 }
 
