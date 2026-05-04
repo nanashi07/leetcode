5eda3940d4cda2cf8a4f107b909e02cc62c7f6f1
Bruce Tsai <bruce.tsai@opennet.tw>
2026-05-04 00:23:51 +0000

    Problem 48

--- tests/medium/rotate_image.rs
+++ tests/medium/rotate_image.rs
@@ -5,7 +5,19 @@ struct Solution;
 
 impl Solution {
     pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
-        todo!()
+        let n = matrix.len();
+        // Transpose
+        for i in 0..n {
+            for j in i + 1..n {
+                let tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        // Reverse each row
+        for row in matrix.iter_mut() {
+            row.reverse();
+        }
     }
 }
 
