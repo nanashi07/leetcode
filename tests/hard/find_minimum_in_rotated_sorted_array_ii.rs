c969c9f28ce9bec43606f63a56d5049b9b62112e
Bruce Tsai <bruce.tsai@opennet.tw>
2026-05-16 00:07:39 +0000

    Problem 154

--- tests/hard/find_minimum_in_rotated_sorted_array_ii.rs
+++ tests/hard/find_minimum_in_rotated_sorted_array_ii.rs
@@ -5,7 +5,22 @@ struct Solution;
 
 impl Solution {
     pub fn find_min(nums: Vec<i32>) -> i32 {
-        todo!()
+        let mut left = 0usize;
+        let mut right = nums.len() - 1;
+
+        while left < right {
+            let mid = left + (right - left) / 2;
+
+            if nums[mid] < nums[right] {
+                right = mid;
+            } else if nums[mid] > nums[right] {
+                left = mid + 1;
+            } else {
+                right -= 1;
+            }
+        }
+
+        nums[left]
     }
 }
 
