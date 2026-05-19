296c40d8d067ad85ce3ee8dacd844a72bf18b91e
Bruce Tsai <bruce.tsai@opennet.tw>
2026-05-19 01:36:59 +0000

    Problem 2540

--- tests/easy/minimum_common_value.rs
+++ tests/easy/minimum_common_value.rs
@@ -5,7 +5,20 @@ struct Solution;
 
 impl Solution {
     pub fn get_common(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
-        todo!()
+        let mut i1 = 0;
+        let mut i2 = 0;
+        while i1 < nums1.len() && i2 < nums2.len() {
+            let n1 = nums1[i1];
+            let n2 = nums2[i2];
+            if n1 == n2 {
+                return n1;
+            } else if n1 < n2 {
+                i1 += 1;
+            } else {
+                i2 += 1;
+            }
+        }
+        -1
     }
 }
 
