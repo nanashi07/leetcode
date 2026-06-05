1e2bd2f0409974587aa9f1deac53a699b6f64606
Bruce Tsai <bruce.tsai@opennet.tw>
2026-06-05 00:27:46 +0000

    Problem 3753

--- tests/hard/total_waviness_of_numbers_in_range_ii.rs
+++ tests/hard/total_waviness_of_numbers_in_range_ii.rs
@@ -5,7 +5,69 @@ struct Solution;
 
 impl Solution {
     pub fn total_waviness(num1: i64, num2: i64) -> i64 {
-        todo!()
+        fn solve(x: i64) -> i64 {
+            if x <= 0 {
+                return 0;
+            }
+
+            let digits: Vec<u8> = x
+                .to_string()
+                .bytes()
+                .map(|b| b - b'0')
+                .collect();
+            let n = digits.len();
+
+            let mut memo: Vec<Vec<Vec<[Option<(i64, i64)>; 11]>>> =
+                vec![vec![vec![[None; 11]; 2]; 2]; n + 1];
+
+            fn dfs(
+                pos: usize,
+                tight: usize,
+                started: usize,
+                prev: usize,
+                digits: &[u8],
+                memo: &mut [Vec<Vec<[Option<(i64, i64)>; 11]>>],
+            ) -> (i64, i64) {
+                if pos == digits.len() {
+                    return (1, 0);
+                }
+
+                if let Some(v) = memo[pos][tight][started][prev] {
+                    return v;
+                }
+
+                let limit = if tight == 1 { digits[pos] as usize } else { 9 };
+                let mut total_count = 0i64;
+                let mut total_sum = 0i64;
+
+                for d in 0..=limit {
+                    let next_tight = if tight == 1 && d == limit { 1 } else { 0 };
+
+                    if started == 0 && d == 0 {
+                        let (cnt, s) = dfs(pos + 1, next_tight, 0, 10, digits, memo);
+                        total_count += cnt;
+                        total_sum += s;
+                    } else {
+                        let next_started = 1;
+                        let next_prev = d;
+                        let (cnt, s) = dfs(pos + 1, next_tight, next_started, next_prev, digits, memo);
+
+                        let add = if started == 1 && prev != d { cnt } else { 0 };
+                        total_count += cnt;
+                        total_sum += s + add;
+                    }
+                }
+
+                let res = (total_count, total_sum);
+                memo[pos][tight][started][prev] = Some(res);
+                res
+            }
+
+            let (_, sum) = dfs(0, 1, 0, 10, &digits, &mut memo);
+            sum
+        }
+
+        solve(num2) - solve(num1 - 1)
     }
 }
 
