0b8efa21bbe9484ee050a17a8b390eb9e12bbb65
Bruce Tsai <bruce.tsai@opennet.tw>
2026-06-08 00:08:21 +0000

    Problem 2161

---
+++ tests/medium/partition_array_according_to_given_pivot.rs
@@ -0,0 +1,32 @@
+// 2161. Partition Array According to Given Pivot
+// https://leetcode.com/problems/partition-array-according-to-given-pivot/
+
+struct Solution;
+
+impl Solution {
+    pub fn pivot_array(nums: Vec<i32>, pivot: i32) -> Vec<i32> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::medium::partition_array_according_to_given_pivot::Solution;
+
+    #[test]
+    fn test_pivot_array_1() {
+        let nums = [9, 12, 5, 10, 14, 3, 10].to_vec();
+        let pivot = 10;
+        assert_eq!(
+            [9, 5, 3, 10, 10, 12, 14].to_vec(),
+            Solution::pivot_array(nums, pivot)
+        );
+    }
+
+    #[test]
+    fn test_pivot_array_2() {
+        let nums = [-3, 4, 3, 2].to_vec();
+        let pivot = 2;
+        assert_eq!([-3, 2, 4, 3].to_vec(), Solution::pivot_array(nums, pivot));
+    }
+}
