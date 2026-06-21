ea39d9d9a89d9911830bfd76f2e2c9e0f58745d3
Bruce Tsai <bruce.tsai@opennet.tw>
2026-06-21 00:18:52 +0000

    Problem 1833

--- tests/medium/maximum_ice_cream_bars.rs
+++ tests/medium/maximum_ice_cream_bars.rs
@@ -5,7 +5,16 @@ struct Solution;
 
 impl Solution {
     pub fn max_ice_cream(costs: Vec<i32>, coins: i32) -> i32 {
-        todo!()
+        let mut costs = costs;
+        costs.sort_unstable();
+        let mut coins = coins;
+        let mut count = 0;
+        for cost in costs {
+            if coins < cost { break; }
+            coins -= cost;
+            count += 1;
+        }
+        count
     }
 }
 
