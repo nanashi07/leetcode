b38c5804ff1df84b5d3248c9e5a17b41ef9d4225
Bruce Tsai <bruce.tsai@opennet.tw>
2026-06-22 03:16:14 +0000

    Problem 1189

--- tests/easy/maximum_number_of_balloons.rs
+++ tests/easy/maximum_number_of_balloons.rs
@@ -1,11 +1,30 @@
 // 1189. Maximum Number of Balloons
 // https://leetcode.com/problems/maximum-number-of-balloons/
 
+use std::collections::HashMap;
+
 struct Solution;
 
 impl Solution {
     pub fn max_number_of_balloons(text: String) -> i32 {
-        todo!()
+        let mut balloon_map: HashMap<char, i32> = HashMap::new();
+        for c in "balloon".chars() {
+            *balloon_map.entry(c).or_insert(0) += 1;
+        }
+        let mut map: HashMap<char, i32> = HashMap::new();
+        for c in text.chars() {
+            *map.entry(c).or_insert(0) += 1;
+        }
+        let mut min: f64 = i32::MAX as f64;
+        for (k, v) in &balloon_map {
+            if map.contains_key(k) {
+                let t = map[k];
+                min = min.min(t as f64 / *v as f64);
+            } else {
+                return 0;
+            }
+        }
+        min as i32
     }
 }
 
