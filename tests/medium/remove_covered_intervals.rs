c22095605caace7f6a2ef2efe10cb9afcc219a48
Bruce Tsai <bruce.tsai@opennet.tw>
2026-07-06 00:49:48 +0000

    Problem 1288

--- tests/medium/remove_covered_intervals.rs
+++ tests/medium/remove_covered_intervals.rs
@@ -4,8 +4,18 @@
 struct Solution;
 
 impl Solution {
-    pub fn remove_covered_intervals(intervals: Vec<Vec<i32>>) -> i32 {
-        todo!()
+    pub fn remove_covered_intervals(mut intervals: Vec<Vec<i32>>) -> i32 {
+        intervals.sort_unstable_by(|a, b| a[0].cmp(&b[0]).then_with(|| b[1].cmp(&a[1])));
+
+        let mut count = 0;
+        let mut max_end = i32::MIN;
+        for interval in intervals {
+            if interval[1] > max_end {
+                count += 1;
+                max_end = interval[1];
+            }
+        }
+        count
     }
 }
 
