7f0ab7341d46ac4d1048432edffeba83f184654e
Bruce Tsai <bruce.tsai@opennet.tw>
2026-07-21 00:14:08 +0000

    Problem 3499

--- tests/medium/maximize_active_section_with_trade_i.rs
+++ tests/medium/maximize_active_section_with_trade_i.rs
@@ -5,7 +5,31 @@ struct Solution;
 
 impl Solution {
     pub fn max_active_sections_after_trade(s: String) -> i32 {
-        todo!()
+        let s = s.as_bytes();
+        let n = s.len();
+        let mut ans = 0;
+        let mut i = 0;
+        let mut pre: Option<i32> = None;
+        let mut mx = 0;
+
+        while i < n {
+            let mut j = i + 1;
+            while j < n && s[j] == s[i] {
+                j += 1;
+            }
+            let cur = (j - i) as i32;
+            if s[i] == b'1' {
+                ans += cur;
+            } else {
+                if let Some(p) = pre {
+                    mx = mx.max(p + cur);
+                }
+                pre = Some(cur);
+            }
+            i = j;
+        }
+
+        ans + mx
     }
 }
 
