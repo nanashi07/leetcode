5defbd48f8ac14c9c677a9f178c877a19571eb59
Bruce Tsai <bruce.tsai@opennet.tw>
2026-08-01 00:38:20 +0000

    Problem 486

--- tests/medium/predict_the_winner.rs
+++ tests/medium/predict_the_winner.rs
@@ -5,11 +5,22 @@ struct Solution;
 
 impl Solution {
     pub fn predict_the_winner(nums: Vec<i32>) -> bool {
-        todo!()
+        let n = nums.len();
+        if n & 1 == 0 {
+            return true;
+        }
+        let mut dp = nums.clone();
+        for i in (0..n).rev() {
+            for j in (i + 1)..n {
+                dp[j] = std::cmp::max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        dp[n - 1] >= 0
     }
 }
 
 #[cfg(test)]
+#[allow(clippy::bool_assert_comparison)]
 mod tests {
     use crate::medium::predict_the_winner::Solution;
 
