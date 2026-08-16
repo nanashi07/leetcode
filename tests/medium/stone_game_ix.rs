d5fe52e8d44c6b6c83e3873e06abef0301a57d16
Bruce Tsai <bruce.tsai@opennet.tw>
2026-08-16 00:08:01 +0000

    Problem 2029

--- tests/medium/stone_game_ix.rs
+++ tests/medium/stone_game_ix.rs
@@ -5,7 +5,16 @@ struct Solution;
 
 impl Solution {
     pub fn stone_game_ix(stones: Vec<i32>) -> bool {
-        todo!()
+        let mut counts = [0usize; 3];
+        for stone in stones {
+            counts[(stone % 3) as usize] += 1;
+        }
+
+        if counts[0] % 2 == 0 {
+            counts[1] > 0 && counts[2] > 0
+        } else {
+            counts[1].abs_diff(counts[2]) > 2
+        }
     }
 }
 
