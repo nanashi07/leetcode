7cf035c6f89ecce7b030fba97db89696374c5e14
Bruce Tsai <bruce.tsai@opennet.tw>
2026-08-17 00:29:29 +0000

    Problem 1563

--- tests/hard/stone_game_v.rs
+++ tests/hard/stone_game_v.rs
@@ -5,7 +5,34 @@ struct Solution;
 
 impl Solution {
     pub fn stone_game_v(stone_value: Vec<i32>) -> i32 {
-        todo!()
+        let n = stone_value.len();
+        if n < 2 {
+            return 0;
+        }
+
+        let mut prefix = vec![0; n + 1];
+        for (i, &value) in stone_value.iter().enumerate() {
+            prefix[i + 1] = prefix[i] + value;
+        }
+
+        let mut dp = vec![vec![0; n]; n];
+        for length in 2..=n {
+            for left in 0..=n - length {
+                let right = left + length - 1;
+                for split in left..right {
+                    let left_sum = prefix[split + 1] - prefix[left];
+                    let right_sum = prefix[right + 1] - prefix[split + 1];
+                    let score = if left_sum <= right_sum {
+                        left_sum + dp[left][split]
+                    } else {
+                        right_sum + dp[split + 1][right]
+                    };
+                    dp[left][right] = dp[left][right].max(score);
+                }
+            }
+        }
+
+        dp[0][n - 1]
     }
 }
 
