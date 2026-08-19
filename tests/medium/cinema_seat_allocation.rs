f4cb02ebd0e4a3476d914fbfc14c7c8e3f9273e6
Bruce Tsai <bruce.tsai@opennet.tw>
2026-08-19 00:16:46 +0000

    Problem 1386

--- tests/medium/cinema_seat_allocation.rs
+++ tests/medium/cinema_seat_allocation.rs
@@ -1,11 +1,40 @@
 // 1386. Cinema Seat Allocation
 // https://leetcode.com/problems/cinema-seat-allocation/
 
+use std::collections::HashMap;
+
 struct Solution;
 
 impl Solution {
     pub fn max_number_of_families(n: i32, reserved_seats: Vec<Vec<i32>>) -> i32 {
-        todo!()
+        const LEFT: u8 = 0b0000_1111;
+        const MIDDLE: u8 = 0b0011_1100;
+        const RIGHT: u8 = 0b1111_0000;
+
+        let mut reserved_by_row = HashMap::new();
+        for seat in reserved_seats {
+            let row = seat[0];
+            let number = seat[1];
+            if (2..=9).contains(&number) {
+                *reserved_by_row.entry(row).or_default() |= 1 << (number - 2);
+            }
+        }
+
+        let mut families = 2 * n;
+        for mask in reserved_by_row.values() {
+            let left_available = mask & LEFT == 0;
+            let middle_available = mask & MIDDLE == 0;
+            let right_available = mask & RIGHT == 0;
+            let row_families = if left_available && right_available {
+                2
+            } else if left_available || middle_available || right_available {
+                1
+            } else {
+                0
+            };
+            families -= 2 - row_families;
+        }
+        families
     }
 }
 
