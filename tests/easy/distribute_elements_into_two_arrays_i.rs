c6d2ee7d24f6f37ecc5abd84b210afaff886e0f0
Bruce Tsai <bruce.tsai@opennet.tw>
2026-08-20 01:04:15 +0000

    Problem 3069

--- tests/easy/distribute_elements_into_two_arrays_i.rs
+++ tests/easy/distribute_elements_into_two_arrays_i.rs
@@ -5,7 +5,17 @@ struct Solution;
 
 impl Solution {
     pub fn result_array(nums: Vec<i32>) -> Vec<i32> {
-        todo!()
+        let mut arr1 = vec![nums[0]];
+        let mut arr2 = vec![nums[1]];
+        for &x in &nums[2..] {
+            if arr1.last().unwrap() > arr2.last().unwrap() {
+                arr1.push(x);
+            } else {
+                arr2.push(x);
+            }
+        }
+        arr1.append(&mut arr2);
+        arr1
     }
 }
 
