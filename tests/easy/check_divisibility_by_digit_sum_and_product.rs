6f64714cf300c8e21242fbc964f79ce6e0b57308
Bruce Tsai <bruce.tsai@opennet.tw>
2026-08-22 00:19:37 +0000

    Problem 3622

---
+++ tests/easy/check_divisibility_by_digit_sum_and_product.rs
@@ -0,0 +1,27 @@
+// 3622. Check Divisibility by Digit Sum and Product
+// https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/
+
+struct Solution;
+
+impl Solution {
+    pub fn check_divisibility(n: i32) -> bool {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::easy::check_divisibility_by_digit_sum_and_product::Solution;
+
+    #[test]
+    fn test_check_divisibility_1() {
+        let n = 99;
+        assert!(Solution::check_divisibility(n));
+    }
+
+    #[test]
+    fn test_check_divisibility_2() {
+        let n = 23;
+        assert!(!Solution::check_divisibility(n));
+    }
+}
