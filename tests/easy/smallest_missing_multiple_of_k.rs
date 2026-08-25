f48d491d46e103d5551e192d180b423f3b85dd10
Bruce Tsai <bruce.tsai@opennet.tw>
2026-08-25 00:18:31 +0000

    Problem 3718

--- tests/easy/smallest_missing_multiple_of_k.rs
+++ tests/easy/smallest_missing_multiple_of_k.rs
@@ -7,13 +7,15 @@ impl Solution {
     pub fn missing_multiple(nums: Vec<i32>, k: i32) -> i32 {
         let mut nums = nums
             .into_iter()
+            .collect::<std::collections::HashSet<i32>>()
+            .into_iter()
             .filter(|&n| n >= k && n % k == 0)
             .collect::<Vec<_>>();
         nums.sort_unstable();
 
         for i in 0..nums.len() {
             let n = (i + 1) as i32 * k;
-            if nums[i] > n {
+            if nums[i] != n {
                 return n;
             }
         }
@@ -37,5 +39,22 @@ mod tests {
         let nums = [1, 4, 7, 10, 15].to_vec();
         let k = 5;
         assert_eq!(5, Solution::missing_multiple(nums, k));
+    }
+
+    #[test]
+    fn test_missing_multiple_3() {
+        let nums = [3, 29, 3, 51].to_vec();
+        let k = 3;
+        assert_eq!(6, Solution::missing_multiple(nums, k));
+    }
+
+    #[test]
+    fn test_missing_multiple_4() {
+        let nums = [
+            83, 96, 34, 56, 48, 30, 7, 14, 77, 66, 66, 66, 21, 17, 38, 7, 9,
+        ]
+        .to_vec();
+        let k = 7;
+        assert_eq!(28, Solution::missing_multiple(nums, k));
     }
 }
