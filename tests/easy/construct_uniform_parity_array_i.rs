21ba572762feed4eef75bf244d1fcd3fe7e0622e
Bruce Tsai <bruce.tsai@opennet.tw>
2026-09-02 00:10:59 +0000

    Problem 3875

--- tests/easy/construct_uniform_parity_array_i.rs
+++ tests/easy/construct_uniform_parity_array_i.rs
@@ -4,8 +4,8 @@
 struct Solution;
 
 impl Solution {
-    pub fn uniform_array(nums1: Vec<i32>) -> bool {
-        todo!()
+    pub fn uniform_array(_nums1: Vec<i32>) -> bool {
+        true
     }
 }
 
