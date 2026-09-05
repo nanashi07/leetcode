97b795d00508004fc0a28ad83564d54db422da26
Bruce Tsai <bruce.tsai@opennet.tw>
2026-09-05 00:08:37 +0000

    Problem 3904

--- tests/medium/smallest_stable_index_ii.rs
+++ tests/medium/smallest_stable_index_ii.rs
@@ -5,7 +5,23 @@ struct Solution;
 
 impl Solution {
     pub fn first_stable_index(nums: Vec<i32>, k: i32) -> i32 {
-        todo!()
+        let n = nums.len();
+        if n == 0 {
+            return -1;
+        }
+        let mut min_suffix = vec![0; n];
+        min_suffix[n - 1] = nums[n - 1];
+        for i in (0..n - 1).rev() {
+            min_suffix[i] = min_suffix[i + 1].min(nums[i]);
+        }
+        let mut max_prefix = i32::MIN;
+        for (i, &num) in nums.iter().enumerate() {
+            max_prefix = max_prefix.max(num);
+            if max_prefix - min_suffix[i] <= k {
+                return i as i32;
+            }
+        }
+        -1
     }
 }
 
