ef75af52c103be444fba767a90100f138e1e24f9
Bruce Tsai <bruce.tsai@opennet.tw>
2026-09-06 00:11:12 +0000

    Problem 115

--- tests/hard/distinct_subsequences.rs
+++ tests/hard/distinct_subsequences.rs
@@ -5,7 +5,17 @@ struct Solution;
 
 impl Solution {
     pub fn num_distinct(s: String, t: String) -> i32 {
-        todo!()
+        let (s, t) = (s.as_bytes(), t.as_bytes());
+        let mut dp = vec![0i64; t.len() + 1];
+        dp[0] = 1;
+        for &sc in s {
+            for j in (1..=t.len()).rev() {
+                if sc == t[j - 1] {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        dp[t.len()] as i32
     }
 }
 
