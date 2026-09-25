73ca8bf91c1716f524f710b6f6ea821a7c19fe17
Bruce Tsai <bruce.tsai@opennet.tw>
2026-09-25 00:19:02 +0000

    Problem 1096

--- tests/hard/brace_expansion_ii.rs
+++ tests/hard/brace_expansion_ii.rs
@@ -1,11 +1,66 @@
 // 1096. Brace Expansion II
 // https://leetcode.com/problems/brace-expansion-ii/
 
+use std::collections::BTreeSet;
+
 struct Solution;
 
 impl Solution {
     pub fn brace_expansion_ii(expression: String) -> Vec<String> {
-        todo!()
+        let mut parser = Parser {
+            expression: expression.as_bytes(),
+            position: 0,
+        };
+        parser.parse_union().into_iter().collect()
+    }
+}
+
+struct Parser<'a> {
+    expression: &'a [u8],
+    position: usize,
+}
+
+impl Parser<'_> {
+    fn parse_union(&mut self) -> BTreeSet<String> {
+        let mut result = BTreeSet::new();
+        result.extend(self.parse_concatenation());
+
+        while self.position < self.expression.len() && self.expression[self.position] == b',' {
+            self.position += 1;
+            result.extend(self.parse_concatenation());
+        }
+
+        result
+    }
+
+    fn parse_concatenation(&mut self) -> BTreeSet<String> {
+        let mut result = BTreeSet::from([String::new()]);
+
+        while self.position < self.expression.len()
+            && self.expression[self.position] != b','
+            && self.expression[self.position] != b'}'
+        {
+            let atom = if self.expression[self.position] == b'{' {
+                self.position += 1;
+                let nested = self.parse_union();
+                self.position += 1;
+                nested
+            } else {
+                let character = self.expression[self.position] as char;
+                self.position += 1;
+                BTreeSet::from([character.to_string()])
+            };
+
+            let mut concatenated = BTreeSet::new();
+            for prefix in &result {
+                for suffix in &atom {
+                    concatenated.insert(format!("{prefix}{suffix}"));
+                }
+            }
+            result = concatenated;
+        }
+
+        result
     }
 }
 
